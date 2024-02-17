6f21006a2e3d3ae482843d7acb542e710f612877
Reggie Simon <70024190+ReggieSimon@users.noreply.github.com>
2024-02-17 08:29:29 +0800

    Update WeekDaysActivities.cs

--- WeekDaysActivities.cs
+++ WeekDaysActivities.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeekDaysActrivities : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using System;
 
-    // Update is called once per frame
-    void Update()
-    {
+public class WeekDaysActivities
+{
+	public static void Main()
+	{
+		int day;
         
-    }
+        Console.WriteLine("Enter the week day number : ");
+		day = Convert.ToInt32(Console.ReadLine());
+		
+		switch(day){
+			case 1:
+				Console.WriteLine("Monday");
+				break;
+			case 2:
+				Console.WriteLine("Tuesday");
+				break;
+			case 3:
+				Console.WriteLine("Wednesday");
+				break;
+			case 4:
+				Console.WriteLine("Thursday");
+				break;
+			case 5:
+				Console.WriteLine("Friday");
+				break;
+			case 6:
+				Console.WriteLine("Saturday");
+				break;
+			case 7:
+				Console.WriteLine("Sunday");
+				break;
+			default:
+				Console.WriteLine("Invalid input");
+				break;
+		}
+	}
 }
